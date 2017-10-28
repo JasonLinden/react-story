7758dd02f93ae9ed2f4dea1e85cb00dba7aa29f0
Zach Sherman <zksherm@gmail.com>
2017-10-28 12:55:34 -0400

    only allow class

--- src/components/Story/index.js
+++ src/components/Story/index.js
@@ -17,7 +17,7 @@ export default ({ stories, storyPath }) => {
       <Title size="xl" pb={1} mb={2} borderBottom borderColor='primary1' borderWidth={1}>
         {story.name}
       </Title>
-      {typeof story.component === 'function' ? story.component() : story.component}
+      {story.component}
     </Box>
   )
 }
